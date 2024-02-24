80e6567b5236cd6ce4c62246d56c2de27c0dd745
Egor Matorin <egor.matorin@gmail.com>
2024-02-24 17:23:06 +0300

    add network/service.py | Added function to get local IP

---
+++ network/service.py
@@ -0,0 +1,14 @@
+import socket
+
+
+def get_local_ip():
+    try:
+        # Get the local IP address associated with the first non-localhost network interface
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except Exception as e:
+        print(f"Error getting local IP: {e}")
+        return None
