1da181b379d39ac0ad15dee9e45340391e9d9b30
Egor Matorin <egor.matorin@gmail.com>
2024-02-27 21:07:49 +0300

    upd network/peer.py | Upgrade find_connections()

--- network/peer.py
+++ network/peer.py
@@ -17,7 +17,14 @@ class Peer:
 
         self.addresses = []
         self.active_connection = None
+        self.free_connection = None
         self.connection_address = None
+        # Flags
+        self.is_real_connection = False
+        self.address_is_vacant = False
+        self.address_is_busy = False
+        self.connection_was_lost = False
+        # self.is_reconnect = False
 
         threading.Thread(target=self.find_connections, args=()).start()
 
@@ -26,50 +33,101 @@ class Peer:
             if self.active_connection is None:
                 # subnet = '192.168.0' # LAN
                 subnet = '127.0.0'  # Debug
-                # self.find_free_connections(find_devices_with_port_open(subnet, self.port))
                 addresses = find_devices_with_port_open(subnet, self.port)
-                self.addresses.clear()
-                self.addresses = addresses
                 try:
-                    self.addresses.remove(self.host)
+                    addresses.remove(self.host)
                 except ValueError:
                     pass
+                print(f'[find_connections] Any  {addresses}')
+                if self.active_connection is None:
+                    self.find_free_connections(addresses)
+                print(f'[find_connections] Free {self.addresses}')
 
     def find_free_connections(self, addresses):
         free_addresses = []
-
         if self.active_connection is None:
             for address in addresses:
-                self.connect(address, self.port)
-                time.sleep(0.5)
-                if self.active_connection is not None:
-                    free_addresses.append(address)
-                    self.disconnect(self.active_connection, address)
+                if self.active_connection is None:
+                    self.service_connection(address, self.port)
+                start_time = time.time()
+                while True:
+                    if self.address_is_vacant is True:
+                        free_addresses.append(address)
+                        if self.active_connection is not self.free_connection:
+                            self.disconnect(self.free_connection, address)
+                        break
+                    elif self.address_is_busy:
+                        break
+                    elif time.time() - start_time > 3:
+                        break
 
+            self.address_is_vacant = False
+            self.address_is_busy = False
+
+        self.addresses.clear()
         self.addresses = free_addresses
 
+    def reconnecting_socket(self, port, timeout=5, retries=5):
+        self.connection_was_lost = True
+        for attempt in range(retries):
+            try:
+                sock = socket.create_connection((self, port), timeout)
+                self.connection_was_lost = False
+                return sock
+            except socket.error as e:
+                print(f"Connection attempt {attempt + 1} failed: {e}")
+                if attempt < retries - 1:
+                    time.sleep(2 ** attempt)  # Exponential backoff
+                else:
+                    raise
+
     def connect(self, peer_host, peer_port):
+        self.is_real_connection = True
+        self.service_connection(peer_host, peer_port)
+
+    def service_connection(self, peer_host, peer_port):
         try:
+            # if self.is_real_connection:
+            #     connection = self.reconnecting_socket(peer_host, peer_port)
+            # else:
             connection = socket.create_connection((peer_host, peer_port))
 
-            if self.active_connection is not None:
-                self.disconnect(self.active_connection, self.connection_address)
+            if self.is_real_connection:
+                if self.active_connection is not None:
+                    self.disconnect(self.active_connection, self.connection_address)
 
-            self.active_connection = connection
-            self.connection_address = f'({peer_host}:{peer_port})'
+                self.active_connection = connection
+                self.connection_address = f'({peer_host}:{peer_port})'
+                print(f"[{self.host:}:{self.port}] Connected to {peer_host}:{peer_port}")
 
-            print(f"[{self.host:}:{self.port}] Connected to {peer_host}:{peer_port}")
             time.sleep(1)
             threading.Thread(target=self.handle_client, args=(connection, (peer_host, peer_port))).start()
         except ConnectionRefusedError:
             print(f"[ERROR] Connection refused: {ConnectionRefusedError}")
 
+    # def disconnect(self, connection, address):
+    #     pass
+
     def disconnect(self, connection, address):
+        # print(f'[disconnect] Step 1.0 | is real - {self.is_real_connection}')
+        if not self.is_real_connection:
+            print(f'[disconnect] Step 2.1 | free connect - {self.free_connection}')
+            self.free_connection = None
+
+        print(f'[disconnect] Step 1.1 | is real - {self.is_real_connection} | is vacant {self.address_is_vacant}')
+        self.is_real_connection = False
+        self.address_is_vacant = False
+
         if connection == self.active_connection:
+            print(f'[disconnect] Step 1.0 | active connect - {self.active_connection}')
             self.active_connection = None
             self.connection_address = None
-            print(f"[{self.host:}:{self.port}] Connection from {address} closed.")
-        connection.close()
+
+        print(f"[{self.host:}:{self.port}] Connection from {address} closed.")
+        try:
+            connection.close()
+        except Exception as e:
+            print(f'Close connection ERROR: {e}')
 
     def send_data(self, connection, address, data):
         if self.active_connection is None:
@@ -88,13 +146,16 @@ class Peer:
         print(f"[{self.host:}:{self.port}] Listening for connections on {self.host}:{self.port}")
 
         while True:
-            connection, address = self.socket.accept()
-            if self.active_connection is None:
-                connection.sendall('free'.encode())
-                threading.Thread(target=self.handle_client, args=(connection, address)).start()
-            else:
-                connection.sendall('busy'.encode())
-                # connection.close()
+            try:
+                connection, address = self.socket.accept()
+                if self.active_connection is None:
+                    connection.sendall('free'.encode())
+                    threading.Thread(target=self.handle_client, args=(connection, address)).start()
+                else:
+                    connection.sendall('busy'.encode())
+            except ConnectionResetError:
+                print(f"[{self.host:}:{self.port}] ConnectionResetError: {ConnectionResetError.mro()}")
+                pass
 
     def handle_client(self, connection, address):
         while True:
@@ -105,18 +166,29 @@ class Peer:
 
                 print(f"[{self.host:}:{self.port}] Received data from {address}: {data.decode()}")
 
-                if data.decode() == "bye" or data.decode() == "busy":
+                if data.decode() == "busy":
+                    self.address_is_busy = True
+                    self.disconnect(connection, address)
+                    return
+                elif data.decode() == "bye":
                     self.disconnect(connection, address)
                     return
                 elif data.decode() == 'free':
-                    print(f"[{self.host:}:{self.port}] Accepted connection from {address}")
-                    self.active_connection = connection
-                    self.connection_address = address
-                    self.send_data(connection, address, 'pair?')
+                    if self.is_real_connection:
+                        print(f"[{self.host:}:{self.port}] Accepted connection from {address}")
+                        self.active_connection = connection
+                        self.connection_address = address
+                        self.send_data(connection, address, 'pair?')
+                    else:
+                        self.free_connection = connection
+                    self.address_is_vacant = True
                 elif data.decode() == 'pair?':
                     print(f"[{self.host:}:{self.port}] Accepted connection from {address}")
+                    self.is_real_connection = True
                     self.active_connection = connection
                     self.connection_address = address
+
+                    # Replace here
                     print(f"[{self.host:}:{self.port}] Set connection with {address}? (Y/N)")
                     if input().lower() == "y":
                         self.send_data(connection, address, 'yes')
@@ -161,21 +233,17 @@ if __name__ == "__main__":
 
     node = None
 
-    if args.address is not None:
-        node = Peer(args.address, PORT)
-    else:
+    if args.address is None:
         node = Peer('127.0.0.1', PORT)
+    else:
+        node = Peer(args.address, PORT)
     node.start()
 
     address2 = None
 
     while True:
         msg = input('')
-        if msg == 'connect':
-            print('Enter address to connection: ')
-            address2 = input()
-            node.connect(address2, PORT)
-        elif msg.split(' ')[0] == 'connect':
+        if msg.split(' ')[0] == 'connect':
             try:
                 address2 = msg.split(' ')[1]
                 node.connect(address2, PORT)
@@ -184,6 +252,8 @@ if __name__ == "__main__":
         elif msg == "exit":
             node.stop()
             break
+        elif msg == 'bye':
+            node.disconnect(node.active_connection, node.connection_address)
         elif msg == 'faddr':
             print(node.addresses)
         elif msg == 'fc':
@@ -194,5 +264,4 @@ if __name__ == "__main__":
         elif msg == 'show':
             print(node.active_connection)
         elif msg.split(' ')[0] == 'm':
-            node.send_data(node.active_connection, node.connection_address, msg.split(' ')[1])
-            # pass
+            node.send_data(node.active_connection, node.connection_address, " ".join(msg.split(' ')[1:]))
